d3a94b3dc4e209bcf18964d4b57cc5cf05115f50
Edward <edwardi.reyes21@gmail.com>
2021-08-11 18:44:37 -0700

    Added Book object to represent each book

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+// Book object
+function Book(author, title, pages) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+}
+
+// Default books for debugging
+let harry_potter = new Book("J.K Rowling", "Harry Potter", 500);
+
 // Representing the books in our library
-let books = ["Harry Potter"];
+let books = [harry_potter];
 
 // When "Add Book" button is clicked, then open a window
 // that allows user to type in book name, etc.
 let add_book_button = document.querySelector("#add-book");
 add_book_button.addEventListener("click", (Event) => {
-    updateLibrary();
+    
 });
 
-// Updates what the library shows
-updateLibrary = () => {
+// Creates library using all existing books inside the array
+createLibrary = () => {
     let library = document.querySelector("#library");
 
     for (let i = 0; i < books.length; i++) {
@@ -18,3 +28,12 @@ updateLibrary = () => {
         library.appendChild(new_container);
     }
 }
+
+// Updates library whenever the user adds a new book
+updateLibrary = () => {
+
+}
+
+// Every time a page loads, we create the library, which may
+// already hold existing books
+createLibrary();
